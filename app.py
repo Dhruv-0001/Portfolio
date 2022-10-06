89a53571c55cd2ff0b7131627cdec36ed358e222
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-06 20:58:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-image: url("https://i.gifer.com/Q2v1.gif");
              background-attachment: fixed;
              background-size: cover
          }}
