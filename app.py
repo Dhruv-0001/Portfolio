8c1a2f5fba727622d9514de2cbad15677b44c7e8
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-08 04:16:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,12 @@ col2.image(profile_pic)
 
 st.header("DHRUV  TYAGI")
 
-st.markdown("┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉", unsafe_allow_html=True)
+st.markdown("┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉", unsafe_allow_html=True)
 cola,colb,colc=st.columns(3)
 with colb:
     if st.button("▶ PLAY MY SONG  🎸 "):
         st.video("https://www.youtube.com/watch?v=kVpv8-5XWOI")
-st.markdown("┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉", unsafe_allow_html=True)
+st.markdown("┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉", unsafe_allow_html=True)
 
 st.info("(●'◡'●)....Hello..! I am Just an avid learner who loves to apply Python and Machine learning on real world projects. I am currently deep diving into Data Science and have a keen interest in Blockchain and WEB 3.0 technologies🚀.")
 
