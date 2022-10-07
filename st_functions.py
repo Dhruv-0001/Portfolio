b2b73e8c14231f6e6ccea295b99acd10fc0c6140
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-10-08 03:47:48 +0530

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -37,6 +37,15 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
+    elif icon == 'play':
+        st.video("https://www.youtube.com/watch?v=kVpv8-5XWOI")
+        button_code = f'''
+        <p>
+            <a  class="btn btn-outline-warning  btn-lg btn-block" type="button" aria-pressed="true">
+                
+                {label}
+            </a>
+        </p>'''
     elif icon == '':
         button_code = f'''
         <p>
@@ -45,3 +54,7 @@ def st_button(icon, url, label, iconsize):
             </a>
         </p>'''
     return st.markdown(button_code, unsafe_allow_html=True)
+
+with colb:
+    if st.button("PLAY ▶"):
+        st.video("https://www.youtube.com/watch?v=kVpv8-5XWOI")
