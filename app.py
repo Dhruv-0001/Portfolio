8c5861a3cc91b2b91a2821b7dd961786414a02e7
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-12-24 00:29:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ resume_file = current_dir / "assets" / "DHRUV TYAGI.pdf"
 profile_pic = current_dir / "assets" / "profile-pic (6).png"
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "DHRUV TYAGI | PORTFOLIO"
-PAGE_ICON = "🚀"
+PAGE_TITLE = "DhruvCodes | PORTFOLIO"
+#PAGE_ICON = "🚀"
 NAME = "DHRUV TYAGI"
 EMAIL = "dhruvtyagionly1@gmail.com"
 
-st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+st.set_page_config(page_title=PAGE_TITLE)
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
 #with open(css_file) as f:
@@ -34,24 +34,14 @@ col1, col2, col3 = st.columns(3)
 col2.image(profile_pic)
 
 st.header("DHRUV  TYAGI")
+st.write("📫", EMAIL)
 
 cola,colb,colc=st.columns(3)
 with colb:
-    if st.button("PLAY A BEAUTIFUL SONG 🎸 "):
+    if st.button("Listen A Melody 🎸 "):
         st.video("https://www.youtube.com/watch?v=syFZfO_wfMQ")
 
 st.info("(●'◡'●)....Hello..! I am Just an avid learner who loves to apply Python and Machine learning on real world projects. I am currently deep diving into Data Science and have a keen interest in Blockchain and WEB 3.0 technologies🚀.")
-
-col19,col20 = st.columns(2)
-with col19 :
-  st.download_button(
-      label=" 📄 Download Resume",
-      data=PDFbyte,
-      file_name=resume_file.name,
-      mime="application/octet-stream",
-  )
-with col20 :
-  st.write("📫", EMAIL)
 
 st.image("https://cliply.co/wp-content/uploads/2019/12/371903520_SOCIAL_ICONS_TRANSPARENT_400px.gif")
 
@@ -69,9 +59,9 @@ with colb:
 st.image("https://i.pinimg.com/originals/e5/93/ab/e593ab0589d5f1b389e4dfbcce2bce20.gif")
 
 st_button('', 'https://dhruv-0001-shoe-hype-shoe-hype-8ebvwa.streamlitapp.com/', 'SHOE HYPE〽️', icon_size)
+st_button('', 'https://dhruv-0001-hacktoberfest2022-tech-talk-main-z5l9tc.streamlit.app/', 'TECH TALK', icon_size)
 st_button('', 'https://dhruv-0001-stocks-forecastor-app-gqlm6j.streamlitapp.com/', 'STOCKS FORECASTER💹' , icon_size)
 st_button('', 'https://github.com/Dhruv-0001/POSE_DETECTION', 'POSE DETECTION', icon_size)
-st_button('', 'https://github.com/Dhruv-0001/Reimagined-System', 'REIMAGINED SYSTEM', icon_size)
 
 st.write('\n')
 cola1,colb1,colc1 = st.columns(3)
@@ -90,12 +80,11 @@ st.header("WORK EXPERIENCE")
 st.write('\n')
 st.image("https://www.learninglinksindia.org/public/images/Journey-Boat.gif")
 st.write("\n")
-st.header("INVARIANCE AUTOMATION LTD. | SDE Intern | Nov 2022 - Dec 2022 | IIT Kanpur")
-st.markdown('▢ To Develop an OpenCV and Python based User Interface which ensures the proper alignment of various components on PCB.')
-st.markdown('▢ To implement machine learning models for matching the product with the prototype for building an automated PCB assembly line.')
 st.header("SIEMENS | Research and Development Intern | June 2022 - August 2022 | Kalwa,Mumbai")
-st.markdown('▢ Created a workplan for the Automation of labs. In this an API of labs was created using Mind Connect IOT module.')
-st.markdown('▢ Made a prototype application on Mindsphere cloud using Mendix and did PLC programming of labs on LOGO and TIA portal software')
+st.markdown('• Researched and created the work plan for the Automation of Lab work at the R&D department.')
+st.markdown('• Created API of labs using the Mind Connect IOT 2040 module for controlling the labs remotely.')
+st.markdown('• Prototyped an application on Mindsphere cloud using Mendix and designed PLC programs of 10+ labs on LOGO and TIA software.')
+st_button('', 'https://github.com/Dhruv-0001/Reimagined-System', 'REIMAGINED SYSTEM', icon_size)
 
 st.header("EXTRA-CURRICULAR")
 st.write("\n")
@@ -111,7 +100,7 @@ st.markdown('▢ Working on Building a Job Portal which will provide placement a
 st.subheader("TECHNICAL WRITING")
 st.markdown('▢ Written some articles on Emerging Technologies including Nano Diamond Batteries,Metaverse,etc.')
 st.subheader("SOCIAL MEDIA MANAGEMENT")
-st.markdown('▢ HANDLED THE INSTAGRAM PAGE OF ALUMNI ASSOCIATION AND LINKEDIN PAGE OF VARIOUS COLLEGE SOCITIES.')
+st.markdown('▢ HANDLED THE INSTAGRAM PAGE OF ALUMNI ASSOCIATION AND LINKEDIN PAGE OF VARIOUS COLLEGE SOCIETIES.')
 st.markdown('▢ HAVE KNOWLEDGE OF SOCIAL MEDIA ALGORITHMS AND SEO.')
 st.subheader("ATHLETICS")
 st.markdown('▢ WON MEDAL IN SPRINTING IN STATE CAMP.')
