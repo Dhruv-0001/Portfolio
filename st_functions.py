6b2f40e2118f2d01b1e7838033b3645db703dbac
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2022-12-24 03:41:00 +0530

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -46,3 +46,14 @@ def st_button(icon, url, label, iconsize):
         </p>'''
     return st.markdown(button_code, unsafe_allow_html=True)
 
+embed_component= {'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+        <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="dhruvtyagi15" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/dhruvtyagi15?trk=profile-badge">Dhruv Tyagi</a></div>""", 'medium':"""<div style="overflow-y: scroll; height:500px;"> <div id="retainable-rss-embed" 
+data-rss="https://medium.com/feed/retainable,https://medium.com/feed/data-science-in-your-pocket"
+data-maxcols="3" 
+data-layout="grid"
+data-poststyle="inline" 
+data-readmore="Read the rest" 
+data-buttonclass="btn btn-primary" 
+data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""}
+
+              
